e230354995789803eb2e63d6067f447f765b4501
jedi1983 <ragunan@hotmail.com>
2018-10-07 12:31:11 +0700

    modify

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ def create_app(config_filename):
     app.config.from_object(config_filename)
     
     from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+    app.register_blueprint(api_bp, url_prefix='/api/v1')
 
-    from Model import db
-    db.init_app(app)
+    # from Model import db
+    # db.init_app(app)
 
     return app
 
