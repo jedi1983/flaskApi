d51907af775c76061276dbf27157f437752d8c01
jedi1983 <ragunan@hotmail.com>
2018-10-07 12:16:24 +0700

    wrong file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.Hello import Hello
+from resources.News import News
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
